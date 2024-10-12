6888b816189e852a586f9a11a3e74a6b12066776
007 <dr007cc@gmail.com>
2024-10-12 23:00:40 +0330

    feat: init Taker trait

--- shah/src/lib.rs
+++ shah/src/lib.rs
@@ -6,7 +6,7 @@ pub mod server;
 pub(crate) mod utils;
 
 pub use crate::binary::Binary;
-pub use crate::error::ErrorCode;
+pub use crate::error::{ErrorCode, ClientError};
 pub use models::*;
 
 pub use shah_macros::{api, enum_code, model};
@@ -20,4 +20,8 @@ pub struct Api<T> {
     pub input_size: usize,
     pub output_size: usize,
     pub caller: fn(&mut T, &[u8], &mut [u8]) -> Result<(), ErrorCode>,
+}
+
+pub trait Taker {
+    fn take(&self, order: &[u8]) -> Result<&[u8], ErrorCode>;
 }

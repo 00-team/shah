1b9748f7a417353f7b1dff0d8689f80fcc67360c
007 <dr007cc@gmail.com>
2024-10-27 03:04:22 +0330

    release: 0.1.1

--- shah/src/perms.rs
+++ shah/src/perms.rs
@@ -1,15 +1,42 @@
 pub type Perm = (usize, u8);
 pub trait Perms {
+    type Error;
+
     fn perm_get(&self, perm: Perm) -> bool;
     fn perm_set(&mut self, perm: Perm, value: bool);
+    fn perm_any(&self) -> bool;
+    fn perm_check(&self, perm: Perm) -> Result<(), Self::Error>;
+    fn perm_check_many(&self, perms: &[Perm]) -> Result<(), Self::Error> {
+        for p in perms {
+            self.perm_check(*p)?;
+        }
+
+        Ok(())
+    }
 }
+
 impl Perms for [u8] {
+    type Error = ();
+
+    fn perm_check(&self, perm: Perm) -> Result<(), Self::Error> {
+        if self.perm_get(perm) {
+            Ok(())
+        } else {
+            Err(())
+        }
+    }
+
+    fn perm_any(&self) -> bool {
+        self.iter().any(|v| *v != 0)
+    }
+
     fn perm_get(&self, (byte, bit): Perm) -> bool {
         assert!(self.len() > byte);
         let n = self[byte];
         let f = 1 << bit;
         (n & f) == f
     }
+
     fn perm_set(&mut self, (byte, bit): Perm, value: bool) {
         assert!(self.len() > byte);
         let f = 1 << bit;

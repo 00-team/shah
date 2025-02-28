893e4b87b3362e3967d1addd78dbe0ddcedb146d
007 <dr007cc@gmail.com>
2025-02-28 14:59:52 +0330

    fix: update model proc macro inputs

--- shah-macros/src/lib.rs
+++ shah-macros/src/lib.rs
@@ -45,8 +45,9 @@ pub fn api(args: TokenStream, code: TokenStream) -> TokenStream {
 }
 
 #[proc_macro_attribute]
-pub fn model(args: TokenStream, code: TokenStream) -> TokenStream {
-    model::model(args, code)
+pub fn model(_args: TokenStream, code: TokenStream) -> TokenStream {
+    let item = syn::parse_macro_input!(code as syn::ItemStruct);
+    model::model(item).unwrap_or_else(syn::Error::into_compile_error).into()
 }
 
 #[proc_macro_derive(Entity, attributes(entity))]

5b6c7534015143b21a3b2a001d609352b34dcc9b
007 <dr007cc@gmail.com>
2025-04-24 22:51:42 +0330

    feat(perms): add ALL const

--- shah-macros/src/perms.rs
+++ shah-macros/src/perms.rs
@@ -9,14 +9,20 @@ pub fn perms(code: TokenStream) -> TokenStream {
     let idents = syn::parse_macro_input!(code with IdentList::parse_terminated);
 
     let mut s = TokenStream2::new();
+    let mut all = TokenStream2::new();
+    let mut count = 0usize;
 
     for (idx, ident) in idents.iter().enumerate() {
         if ident.to_string().starts_with('_') {
             continue;
         }
+        count += 1;
         let (byte, bit) = (idx / 8, (idx % 8) as u8);
         quote_into! {s += pub const #ident: #ci::models::Perm = (#byte, #bit); };
+        quote_into! {all += #ident, };
     }
+
+    quote_into! {s += pub const ALL: [#ci::models::Perm; #count] = [#all]; };
 
     s.into()
 }

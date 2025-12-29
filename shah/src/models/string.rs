ec3f1042a852fcaa29c85079c9c7af2b77eb58f6
007 <dr007cc@gmail.com>
2025-12-29 18:05:15 +0330

    fix: impl display for ShahString

--- shah/src/models/string.rs
+++ shah/src/models/string.rs
@@ -4,9 +4,67 @@ pub struct ShahString<const N: usize> {
     inner: [u8; N],
 }
 
+impl<const N: usize> ShahString<N> {
+    pub fn as_str(&self) -> &str {
+        shah::AsUtf8Str::as_utf8_str_null_terminated(&self.inner)
+    }
+
+    pub fn set(&mut self, value: &str) -> bool {
+        if value.is_empty() {
+            self.inner.fill(0);
+            return false;
+        }
+
+        let mut overflow = false;
+        let vlen = value.len();
+        let len = if vlen > N {
+            overflow = true;
+            let mut idx = N;
+            loop {
+                if value.is_char_boundary(idx) {
+                    break idx;
+                }
+                idx -= 1;
+                continue;
+            }
+        } else {
+            vlen
+        };
+
+        self.inner[..len].copy_from_slice(&value.as_bytes()[..len]);
+        if len < N {
+            self.inner[len] = 0;
+        }
+
+        overflow
+    }
+}
+
+impl<const N: usize> From<String> for ShahString<N> {
+    fn from(value: String) -> Self {
+        let mut ss = Self::default();
+        ss.set(&value);
+        ss
+    }
+}
+
+impl<const N: usize> From<&str> for ShahString<N> {
+    fn from(value: &str) -> Self {
+        let mut ss = Self::default();
+        ss.set(value);
+        ss
+    }
+}
+
 impl<const N: usize> Default for ShahString<N> {
     fn default() -> Self {
         Self { inner: [0; N] }
+    }
+}
+
+impl<const N: usize> std::fmt::Display for ShahString<N> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(self.as_str())
     }
 }
 
@@ -49,57 +107,5 @@ impl<'de, const N: usize> serde::Deserialize<'de> for ShahString<N> {
     {
         let str = String::deserialize(deserializer)?;
         Ok(Self::from(str))
-    }
-}
-
-impl<const N: usize> From<String> for ShahString<N> {
-    fn from(value: String) -> Self {
-        let mut ss = Self::default();
-        ss.set(&value);
-        ss
-    }
-}
-
-impl<const N: usize> From<&str> for ShahString<N> {
-    fn from(value: &str) -> Self {
-        let mut ss = Self::default();
-        ss.set(value);
-        ss
-    }
-}
-
-impl<const N: usize> ShahString<N> {
-    pub fn as_str(&self) -> &str {
-        shah::AsUtf8Str::as_utf8_str_null_terminated(&self.inner)
-    }
-
-    pub fn set(&mut self, value: &str) -> bool {
-        if value.is_empty() {
-            self.inner.fill(0);
-            return false;
-        }
-
-        let mut overflow = false;
-        let vlen = value.len();
-        let len = if vlen > N {
-            overflow = true;
-            let mut idx = N;
-            loop {
-                if value.is_char_boundary(idx) {
-                    break idx;
-                }
-                idx -= 1;
-                continue;
-            }
-        } else {
-            vlen
-        };
-
-        self.inner[..len].copy_from_slice(&value.as_bytes()[..len]);
-        if len < N {
-            self.inner[len] = 0;
-        }
-
-        overflow
     }
 }
